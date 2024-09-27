b35cf3a28118a4b8a2bda660e63b093da28ce471
Nimira43 <n.rabone72@aol.com>
2024-09-27 12:45:59 +0100

    Set up lighting

--- script.js
+++ script.js
@@ -14,8 +14,17 @@ const camera = new THREE.PerspectiveCamera(
   1000
 )
 const controls = new OrbitControls( camera, renderer.domElement )
+
+const sun = new THREE.DirectionalLight()
+sun.position.set(1, 2, 3)
+scene.add(sun)
+const ambient = new THREE.AmbientLight()
+ambient.intensity = 0.5
+scene.add(ambient)
+
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff4500 })
+const material = new THREE.MeshStandardMaterial({ color: 0xff4500 })
 const cube = new THREE.Mesh(geometry, material)
 
 scene.add(cube)
@@ -23,5 +32,6 @@ camera.position.z = 5
 controls.update()
 
 function animate() {
+  controls.update()
   renderer.render( scene, camera )
 }
