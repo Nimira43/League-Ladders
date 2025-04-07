126376b4a386cfd5de19712750fd916eb78ab891
Nimira43 <n.rabone72@aol.com>
2025-04-07 21:51:15 +0100

    Setting up basic template for the dynamic HTML content

--- browser.app.js
+++ browser.app.js
@@ -3,3 +3,13 @@ const loadingDom = document.querySelector('.loading-text')
 const formDom = document.querySelector('.task-form')
 const taskInputDom = document.querySelector('.task-input')
 const formAlertDom = document.querySelector('.form-alert')
+
+const showTasks = async () => {
+  // Dynamic HTML Template
+}
+
+showTasks()
+
+// tasksDom.addEventListener
+
+// formDom.addEventListener
