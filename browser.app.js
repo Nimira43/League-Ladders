fcc40774bc2bfb42a36d82f5ffd5d1d3acbd0b0a
Nimira43 <n.rabone72@aol.com>
2025-04-07 21:41:44 +0100

    SEtting up DOM variables

---
+++ browser.app.js
@@ -0,0 +1,5 @@
+const tasksDom = document.querySelector('.tasks')
+const loadingDom = document.querySelector('.loading-text')
+const formDom = document.querySelector('.task-form')
+const taskInputDom = document.querySelector('.task-input')
+const formAlertDom = document.querySelector('.form-alert')
