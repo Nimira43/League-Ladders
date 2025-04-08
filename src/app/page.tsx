458ca4d3ad7f98d0bfa677d346efbd7653f70580
Nimira43 <n.rabone72@aol.com>
2025-04-08 04:59:16 +0100

    Combining Next.js server actions with UI components from NextUI, to streamline the login/logout process for users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from '@nextui-org/react'
+import * as actions from '@/actions'
 
 export default function Home() {
   return (
     <div>
-      <Button>Test Button</Button>
+      <form action={actions.signIn}>
+        <Button type='submit'>Sign In</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type='submit'>Sign Out</Button>
+      </form>
     </div>
   )
 }
