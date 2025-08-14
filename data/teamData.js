e383da6d4bf7eef2d07f2ec42ca35dfa26cc7d30
Nimira43 <n.rabone72@aol.com>
2025-08-14 17:30:48 +0100

    League One data - 1

--- data/teamData.js
+++ data/teamData.js
@@ -1,4 +1,7 @@
 export const divisions = {
+  'League One': 
+    ['AFC Wimbledon', 'Barnsley', 'Blackpool', 'Bolton Wanderers', 'Bradford City', 'Burton Albion','Cheltenham Town', 'Chesterfield', 'Colchester United', 'Crawley Town', 'Crewe Alexandra', 'Fleetwood Town', 'Gillingham', 'Grimsby Town', 'Harrogate Town', 'Milton Keynes Dons', 'Newport County', 'Notts County', 'Oldham Athletic', 'Salford City', 'Shrewsbury Town', 'Swindon Town', 'Tranmere Rovers', 'Walsall'
+  ],
   'League Two': 
     ['Accrington Stanley', 'Barnet', 'Barrow', 'Bristol Rovers', 'Bromley', 'Cambridge United','Cheltenham Town', 'Chesterfield', 'Colchester United', 'Crawley Town', 'Crewe Alexandra', 'Fleetwood Town', 'Gillingham', 'Grimsby Town', 'Harrogate Town', 'Milton Keynes Dons', 'Newport County', 'Notts County', 'Oldham Athletic', 'Salford City', 'Shrewsbury Town', 'Swindon Town', 'Tranmere Rovers', 'Walsall'
   ]
