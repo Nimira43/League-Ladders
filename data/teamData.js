a57bd3552febe2e94eb40f0afe54fcd6a3fa565e
Nimira43 <n.rabone72@aol.com>
2025-08-14 17:45:38 +0100

    Championship data - 2

--- data/teamData.js
+++ data/teamData.js
@@ -1,6 +1,6 @@
 export const divisions = {
   'Championship': 
-    ['Birmingham City', 'Blackburn Rovers', 'Bristol City', 'Charlton Athletic', 'Coventry City', 'Derby County', 'Hull City', 'Ipswich Town', 'Exeter City', 'Huddersfield Town', 'Leyton Orient', 'Lincoln City', 'Luton Town', 'Mansfield Town', 'Northampton Town', 'Peterborough United', 'Plymouth Argyle', 'Port Vale', 'Reading', 'Rotherham United', 'Stevenage', 'Stockport County', 'Wigan Athletic', 'Wycombe Wanderers'
+    ['Birmingham City', 'Blackburn Rovers', 'Bristol City', 'Charlton Athletic', 'Coventry City', 'Derby County', 'Hull City', 'Ipswich Town', 'Leicester City', 'Middlesbrough', 'Millwall', 'Norwich City', 'Oxford United', 'Portsmouth', 'Preston North End', 'Peterborough United', 'Plymouth Argyle', 'Port Vale', 'Reading', 'Rotherham United', 'Stevenage', 'Stockport County', 'Wigan Athletic', 'Wycombe Wanderers'
   ],
   'League One': 
     ['AFC Wimbledon', 'Barnsley', 'Blackpool', 'Bolton Wanderers', 'Bradford City', 'Burton Albion','Cardiff City', 'Doncaster Rovers', 'Exeter City', 'Huddersfield Town', 'Leyton Orient', 'Lincoln City', 'Luton Town', 'Mansfield Town', 'Northampton Town', 'Peterborough United', 'Plymouth Argyle', 'Port Vale', 'Reading', 'Rotherham United', 'Stevenage', 'Stockport County', 'Wigan Athletic', 'Wycombe Wanderers'
