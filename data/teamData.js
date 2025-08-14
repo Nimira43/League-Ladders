842e80a40e5b2cf786e4c90136f7a78bcd4c8392
Nimira43 <n.rabone72@aol.com>
2025-08-14 16:43:17 +0100

    League 2 teams - 2

--- data/teamData.js
+++ data/teamData.js
@@ -1,3 +1,4 @@
 export const divisions = {
-  'League Two': ['Accrington Stanley', 'Barnet', 'Barrow', 'Bristol Rovers', 'Bromley', 'Cambridge United']
+  'League Two': ['Accrington Stanley', 'Barnet', 'Barrow', 'Bristol Rovers', 'Bromley', 'Cambridge United','Cheltenham Town', 'Chesterfield', 'Colchester United', 'Crawley Town', 'Crewe Alexandra', 'Fleetwood Town'
+  ]
 }
