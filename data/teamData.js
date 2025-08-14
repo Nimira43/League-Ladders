70824ba1dad2bfa23bf9e70ff4a260f83362c540
Nimira43 <n.rabone72@aol.com>
2025-08-14 16:48:06 +0100

    League Two data completed.

--- data/teamData.js
+++ data/teamData.js
@@ -1,4 +1,5 @@
 export const divisions = {
-  'League Two': ['Accrington Stanley', 'Barnet', 'Barrow', 'Bristol Rovers', 'Bromley', 'Cambridge United','Cheltenham Town', 'Chesterfield', 'Colchester United', 'Crawley Town', 'Crewe Alexandra', 'Fleetwood Town', 'Gillingham', 'Grimsby Town', 'Harrogate Town', 'Milton Keynes Dons', 'Newport County',
+  'League Two': 
+    ['Accrington Stanley', 'Barnet', 'Barrow', 'Bristol Rovers', 'Bromley', 'Cambridge United','Cheltenham Town', 'Chesterfield', 'Colchester United', 'Crawley Town', 'Crewe Alexandra', 'Fleetwood Town', 'Gillingham', 'Grimsby Town', 'Harrogate Town', 'Milton Keynes Dons', 'Newport County', 'Notts County', 'Oldham Athletic', 'Salford City', 'Shrewsbury Town', 'Swindon Town', 'Tranmere Rovers', 'Walsall'
   ]
 }
