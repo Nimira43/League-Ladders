b2259126b0ff91967c2a9a92b12f57350e7ca383
Nimira43 <n.rabone72@aol.com>
2025-08-14 19:07:36 +0100

    Archived current version

---
+++ archive/version-1/data/teamData.js
@@ -0,0 +1,17 @@
+export const divisions = {
+  'Premier League': 
+    ['Arsenal', 'Aston Villa', 'Bournemouth', 'Brentford', 'Brighton & Hove Albion', 'Burnley', 'Chelsea', 'Crystal Palace', 'Everton', 'Fulham', 'Leeds United', 'Liverpool', 'Manchester City', 'Manchester United', 'Newcastle United', 'Nottingham Forest', 'Sunderland', 'Tottenham Hotspur', 'West Ham United', 'Wolverhampton Wanderers'
+  ],
+  'Championship': 
+    ['Birmingham City', 'Blackburn Rovers', 'Bristol City', 'Charlton Athletic', 'Coventry City', 'Derby County', 'Hull City', 'Ipswich Town', 'Leicester City', 'Middlesbrough', 'Millwall', 'Norwich City', 'Oxford United', 'Portsmouth', 'Preston North End', 'Queens Park Rangers', 'Sheffield United', 'Sheffield Wednesday', 'Southampton', 'Stoke City', 'Swansea City', 'Watford', 'West Bromwich Albion', 'Wrexham'
+  ],
+  'League One': 
+    ['AFC Wimbledon', 'Barnsley', 'Blackpool', 'Bolton Wanderers', 'Bradford City', 'Burton Albion','Cardiff City', 'Doncaster Rovers', 'Exeter City', 'Huddersfield Town', 'Leyton Orient', 'Lincoln City', 'Luton Town', 'Mansfield Town', 'Northampton Town', 'Peterborough United', 'Plymouth Argyle', 'Port Vale', 'Reading', 'Rotherham United', 'Stevenage', 'Stockport County', 'Wigan Athletic', 'Wycombe Wanderers'
+  ],
+  'League Two': 
+    ['Accrington Stanley', 'Barnet', 'Barrow', 'Bristol Rovers', 'Bromley', 'Cambridge United','Cheltenham Town', 'Chesterfield', 'Colchester United', 'Crawley Town', 'Crewe Alexandra', 'Fleetwood Town', 'Gillingham', 'Grimsby Town', 'Harrogate Town', 'Milton Keynes Dons', 'Newport County', 'Notts County', 'Oldham Athletic', 'Salford City', 'Shrewsbury Town', 'Swindon Town', 'Tranmere Rovers', 'Walsall'
+  ],
+  'National League': 
+    ['Aldershot Town', 'Altrincham', 'Boreham Wood', 'Boston United', 'Brackley Town', 'Braintree Town','Carlisle United', 'Eastleigh', 'FC Halifax Town', 'Forest Green Rovers', 'Gateshead', 'Hartlepool United', 'Morecambe', 'Rochdale', 'Scunthorpe United', 'Solihull Moors', 'Southend United', 'Sutton United', 'Tamworth', 'Truro City', 'Wealdstone', 'Woking', 'Yeovil Town', 'York City'
+  ]
+}
