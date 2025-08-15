380a6d37b0e72bbd68c1d8442f3a77bd76ef8a90
Nick Rabone <60065507+Nimira43@users.noreply.github.com>
2025-08-15 12:41:30 +0100

    Create teamColours.js. Premier League teams first.

---
+++ data/teamColours.js
@@ -0,0 +1,23 @@
+const premierLeagueTeams = [
+  { team: 'Arsenal', main_colour: 'red', support_colour: 'white' },
+  { team: 'Aston Villa', main_colour: 'claret', support_colour: 'blue' },
+  { team: 'Bournemouth', main_colour: 'red', support_colour: 'black' },
+  { team: 'Brentford', main_colour: 'red', support_colour: 'white' },
+  { team: 'Brighton & Hove Albion', main_colour: 'blue', support_colour: 'white' },
+  { team: 'Burnley', main_colour: 'claret', support_colour: 'blue' },
+  { team: 'Chelsea', main_colour: 'blue', support_colour: 'white' },
+  { team: 'Crystal Palace', main_colour: 'red', support_colour: 'blue' },
+  { team: 'Everton', main_colour: 'blue', support_colour: 'white' },
+  { team: 'Fulham', main_colour: 'white', support_colour: 'black' },
+  { team: 'Liverpool', main_colour: 'red', support_colour: 'white' },
+  { team: 'Luton Town', main_colour: 'orange', support_colour: 'navy' },
+  { team: 'Manchester City', main_colour: 'sky blue', support_colour: 'white' },
+  { team: 'Manchester United', main_colour: 'red', support_colour: 'black' },
+  { team: 'Newcastle United', main_colour: 'black', support_colour: 'white' },
+  { team: 'Nottingham Forest', main_colour: 'red', support_colour: 'white' },
+  { team: 'Sheffield United', main_colour: 'red', support_colour: 'white' },
+  { team: 'Tottenham Hotspur', main_colour: 'white', support_colour: 'navy' },
+  { team: 'West Ham United', main_colour: 'claret', support_colour: 'blue' },
+  { team: 'Wolverhampton Wanderers', main_colour: 'gold', support_colour: 'black' }
+]
+
