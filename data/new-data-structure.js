f5ee70967310c87ce699348aad09e9d2210e802f
Nimira43 <n.rabone72@aol.com>
2025-08-16 16:45:49 +0100

    Updating new-data-structure

--- data/new-data-structure.js
+++ data/new-data-structure.js
@@ -1,24 +1,60 @@
-export const divisions = {
+export let divisions = {
   'Premier League': [
-    {
-      name: 'Arsenal',
-      main_colour: 'red',
-      support_colour: 'white',
-    },
-    {
-      name: 'Aston Villa',
-      main_colour: 'claret',
-      support_colour: 'blue',
-    },
-    // ...
+    { team: 'Arsenal', main_colour: 'red', support_colour: 'white' },
+    { team: 'Aston Villa', main_colour: 'claret', support_colour: 'blue' },
+    { team: 'Bournemouth', main_colour: 'red', support_colour: 'black' },
+    { team: 'Brentford', main_colour: 'red', support_colour: 'white' },
+    { team: 'Brighton & Hove Albion', main_colour: 'blue', support_colour: 'white' },
+    { team: 'Burnley', main_colour: 'claret', support_colour: 'blue' },
+    { team: 'Chelsea', main_colour: 'blue', support_colour: 'white' },
+    { team: 'Crystal Palace', main_colour: 'red', support_colour: 'blue' },
+    { team: 'Everton', main_colour: 'blue', support_colour: 'white' },
+    { team: 'Fulham', main_colour: 'white', support_colour: 'black' },
+    { team: 'Liverpool', main_colour: 'red', support_colour: 'white' },
+    { team: 'Luton Town', main_colour: 'orange', support_colour: 'navy' },
+    { team: 'Manchester City', main_colour: 'sky blue', support_colour: 'white' },
+    { team: 'Manchester United', main_colour: 'red', support_colour: 'black' },
+    { team: 'Newcastle United', main_colour: 'black', support_colour: 'white' },
+    { team: 'Nottingham Forest', main_colour: 'red', support_colour: 'white' },
+    { team: 'Sheffield United', main_colour: 'red', support_colour: 'white' },
+    { team: 'Tottenham Hotspur', main_colour: 'white', support_colour: 'navy' },
+    { team: 'West Ham United', main_colour: 'claret', support_colour: 'blue' },
+    { team: 'Wolverhampton Wanderers', main_colour: 'gold', support_colour: 'black' }
   ],
   'Championship': [
-    {
-      name: 'Birmingham City',
-      main_colour: 'blue',
-      support_colour: 'white',
-    },
-    // ...
+  
   ],
-  // Continue for League One, League Two, etc.
+  'League One': [
+  
+  ],
+  'League Two': [
+  
+  ],
+  'National League': [
+  
+  ],
+  'National North': [
+  
+  ],
+  'National South': [
+  
+  ],
+  'Scottish Premiership': [
+  
+  ],
+  'Scottish Championship': [
+  
+  ],
+  'Scottish League One': [
+  
+  ],
+  'Scottish League Two': [
+  
+  ],
+  'Highland League': [
+  
+  ],
+  'Lowland League': [
+  
+  ],
 }
