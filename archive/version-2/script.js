34d9bff14cb3a30373d40f2273f9a9622fa7160f
Nimira43 <n.rabone72@aol.com>
2025-08-16 14:46:44 +0100

    Archived Version 2

---
+++ archive/version-2/script.js
@@ -0,0 +1,37 @@
+import { divisions } from './data/teamData.js'
+
+for (const [division, teams] of Object.entries(divisions)) {
+  const ul = document.querySelector(`
+    #${division.toLowerCase().replace(/ /g, '-')}
+  `)?.querySelector('ul')
+    
+  if (!ul) continue
+
+  teams.forEach(team => {
+    const li = document.createElement('li')
+    li.textContent = team
+    li.draggable = true
+    li.addEventListener('dragstart', e => {
+      e.dataTransfer.setData('text/plain', team)
+      e.dataTransfer.effectAllowed = 'move'
+      li.classList.add('dragging')
+    })
+    li.addEventListener('dragend', () => li.classList.remove('dragging'))
+    ul.appendChild(li)
+  })
+
+  ul.addEventListener('dragover', e => {
+    e.preventDefault()
+    const dragging = document.querySelector('.dragging')
+    const afterElement = [...ul.querySelectorAll('li')].find(el => {
+      const box = el.getBoundingClientRect()
+      return e.clientY < box.top + box.height / 2
+    })
+
+    if (afterElement) {
+      ul.insertBefore(dragging, afterElement)
+    } else {
+      ul.appendChild(dragging)
+    }
+  })
+}
