be1a9aa8e5301309c9d3737869e3cb1ca79964e8
Nimira43 <n.rabone72@aol.com>
2025-08-16 17:01:57 +0100

    Created colourMap.js

---
+++ data/colourMap.js
@@ -0,0 +1,11 @@
+export const colourMap = {
+  red: '#EF0107',
+  white: '#FFFFFF',
+  blue: '#034694',
+  claret: '#7A263A',
+  black: '#000000',
+  orange: '#F37021',
+  navy: '#001F5B',
+  'sky blue': '#87CEEB',
+  gold: '#FFD700'
+}
