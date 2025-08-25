028b3554e82da0c9acc2120ce3e73d8987ea71a4
Nimira43 <n.rabone72@aol.com>
2025-08-25 01:59:15 +0100

    Archiving team colours version

---
+++ archive/version-6-team-colours/data/rules.js
@@ -0,0 +1,60 @@
+export const divisionRules = {
+  'Premier League': {
+    relegated: [18, 19, 20],
+  },
+  'Championship': {
+    promoted: [1, 2],
+    playoffs: [3, 4, 5, 6],
+    relegated: [22, 23, 24],
+  },
+  'League One': {
+    promoted: [1, 2],
+    playoffs: [3, 4, 5, 6],
+    relegated: [21, 22, 23, 24],
+  },
+  'League Two': {
+    promoted: [1, 2, 3],
+    playoffs: [4, 5, 6, 7],
+    relegated: [23, 24],
+  },
+  'National League': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 5, 6, 7],
+    relegated: [21, 22, 23, 24],
+  },
+  'National North': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 5, 6, 7],
+    relegated: [21, 22, 23, 24],
+  },
+  'National South': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 5, 6, 7],
+    relegated: [21, 22, 23, 24],
+  },
+  'Scottish Premiership': {
+    playoffs: [11],
+    relegated: [12],
+  },
+  'Scottish Championship': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 9],
+    relegated: [10],
+  },
+  'Scottish League One': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 9],
+    relegated: [10],
+  },
+  'Scottish League Two': {
+    promoted: [1],
+    playoffs: [2, 3, 4],
+    relegated: [10],
+  },
+  'Highland League': {
+    promoted: [1],
+  },
+  'Lowland League': {
+    promoted: [1],
+  },
+}
---
+++ archive/version-6-team-colours/script.js
@@ -0,0 +1,70 @@
+import { divisionsV2 } from './data/teamData-v2.js'
+import { divisionRules } from './data/rules.js'
+import { colourMap } from './data/colourMap.js'
+
+const normaliseId = name => name.toLowerCase().replace(/ /g, '-')
+const normaliseColourKey = str => str?.toLowerCase().replace(/ /g, '_')
+const getHex = colour => colourMap[normaliseColourKey(colour)] || colour || '#999'
+
+const applyStatusClass = (li, position, rules) => {
+  li.classList.remove('promoted', 'playoffs', 'relegated')
+  if (rules.promoted?.includes(position)) li.classList.add('promoted')
+  if (rules.playoffs?.includes(position)) li.classList.add('playoffs')
+  if (rules.relegated?.includes(position)) li.classList.add('relegated')
+}
+
+for (const [division, teams] of Object.entries(divisionsV2)) {
+  const ul = document.querySelector(`#${normaliseId(division)}`)?.querySelector('ul')
+  if (!ul) continue
+
+  const rules = divisionRules[division] || {}
+
+  teams.forEach((teamObj, index) => {
+    const { team, main_colour, support_colour } = teamObj
+    const li = document.createElement('li')
+    li.textContent = team
+    li.draggable = true
+    li.style.backgroundColor = getHex(main_colour)
+    li.style.color = getHex(support_colour)
+
+    applyStatusClass(li, index + 1, rules)
+
+    li.addEventListener('dragstart', e => {
+      e.dataTransfer.setData('text/plain', team)
+      e.dataTransfer.effectAllowed = 'move'
+      li.classList.add('dragging')
+    })
+
+    li.addEventListener('dragend', () => li.classList.remove('dragging'))
+
+    ul.appendChild(li)
+  })
+
+  ul.addEventListener('dragover', e => {
+    e.preventDefault()
+    const dragging = document.querySelector('.dragging')
+    const afterElement = [...ul.querySelectorAll('li')].find(el => {
+      const box = el.getBoundingClientRect()
+      return e.clientY < box.top + box.height / 2
+    })
+
+    if (afterElement) {
+      ul.insertBefore(dragging, afterElement)
+    } else {
+      ul.appendChild(dragging)
+    }
+  })
+
+  ul.addEventListener('drop', () => {
+    const items = [...ul.querySelectorAll('li')]
+    items.forEach((li, index) => {
+      const teamName = li.textContent
+      const teamObj = teams.find(t => t.team === teamName)
+      if (!teamObj) return
+
+      li.style.backgroundColor = getHex(teamObj.main_colour)
+      li.style.color = getHex(teamObj.support_colour)
+      applyStatusClass(li, index + 1, rules)
+    })
+  })
+}
