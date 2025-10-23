674c4fdbac5dc674a21b378338bfa6a86bc6abdc
Nimira43 <n.rabone72@aol.com>
2025-10-23 20:18:58 +0100

    Copied over data files

---
+++ league-ladders/src/data/rules.js
@@ -0,0 +1,73 @@
+export const divisionRules = {
+  'Premier League': {
+    relegated: [18, 19, 20],
+    breaks: [17]
+  },
+  'Championship': {
+    promoted: [1, 2],
+    playoffs: [3, 4, 5, 6],
+    relegated: [22, 23, 24],
+    breaks: [2, 6, 21]
+  },
+  'League One': {
+    promoted: [1, 2],
+    playoffs: [3, 4, 5, 6],
+    relegated: [21, 22, 23, 24],
+    breaks: [2, 6, 20]
+  },
+  'League Two': {
+    promoted: [1, 2, 3],
+    playoffs: [4, 5, 6, 7],
+    relegated: [23, 24],
+    breaks: [3, 7, 22]
+  },
+  'National League': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 5, 6, 7],
+    relegated: [21, 22, 23, 24],
+    breaks: [1, 7, 20]
+  },
+  'National North': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 5, 6, 7],
+    relegated: [21, 22, 23, 24],
+    breaks: [1, 7, 20]
+  },
+  'National South': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 5, 6, 7],
+    relegated: [21, 22, 23, 24],
+    breaks: [1, 7, 20]
+  },
+  'Scottish Premiership': {
+    playoffs: [11],
+    relegated: [12],
+    breaks: [10, 11]
+  },
+  'Scottish Championship': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 9],
+    relegated: [10],
+    breaks: [1, 4, 8, 9]
+  },
+  'Scottish League One': {
+    promoted: [1],
+    playoffs: [2, 3, 4, 9],
+    relegated: [10],
+    breaks: [1, 4, 8, 9]
+  },
+  'Scottish League Two': {
+    promoted: [1],
+    playoffs: [2, 3, 4],
+    relegated: [10],
+    breaks: [1, 4, 9]
+  },
+  'Highland League': {
+    promoted: [1],
+    breaks: [1]
+  },
+  'Lowland League': {
+    promoted: [1],
+    breaks: [1]
+  },
+}
