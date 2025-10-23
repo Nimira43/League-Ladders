b823718d945bfe561abbf5d7605620fbf28016e5
Nimira43 <n.rabone72@aol.com>
2025-10-23 22:06:55 +0100

    Team colours set up in tailwind-config

--- league-ladders/tailwind.config.js
+++ league-ladders/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'team-red': '#d71920',
+        'team-white': '#ffffff',
+        'team-blue': '#005bac',
+        'team-black': '#000000',
+        'team-claret': '#7f1734',
+        'team-darkblue': '#00008b',
+        'team-skyblue': '#6fc3f7',
+        'team-orange': '#f4793b',
+        'team-navy': '#1a2b4c',
+        'team-gold': '#fdb913',
+        'team-yellow': '#ffff00',
+        'team-green': '#007a33',
+        'team-maroon': '#800000',
+        'team-amber': '#ffbf00',
+        'team-tangerine': '#f28500'
+      },
+    },
   },
   plugins: [],
 }
