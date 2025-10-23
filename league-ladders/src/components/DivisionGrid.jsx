45208b84a2a059972d86d947ece011670853a904
Nimira43 <n.rabone72@aol.com>
2025-10-23 22:31:36 +0100

    Setting up DivisionGrid

--- league-ladders/src/components/DivisionGrid.jsx
+++ league-ladders/src/components/DivisionGrid.jsx
@@ -1,7 +1,22 @@
+import divisions from '../data/teamData'
+import DivisionCard from './DivisionCard'
+import { divisionRules } from '../data/rules'
+
 function DivisionGrid() {
   return (
-    <div>
-      Division Grid
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4'>
+      {Object
+        .entries(divisions)
+        .map(
+          ([divisionName, teams]) => (
+            <DivisionCard 
+              key={divisionName}
+              divisionName={divisionName}
+              teams={teams}
+              rules={divisionRules[divisionName] || {}}
+            />
+          ))  
+      }
     </div>
   )
 }
