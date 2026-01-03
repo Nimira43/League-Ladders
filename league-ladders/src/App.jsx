a9d91ece917f6f214da455c8e6d588377910e278
Nimira43 <n.rabone72@aol.com>
2026-01-03 11:44:02 +0000

    Project completed.

--- league-ladders/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      League Ladders
-    </div>
-  )
-}
-
-export default App
--- league-ladders/src/components/ZoneLine.jsx
+++
@@ -1,9 +0,0 @@
-function ZoneLine({ label }) {
-  return (
-    <div className="zone-line flex items-center justify-center text-xs text-white font-bold">
-      {label}
-    </div>
-  )
-}
-
-export default ZoneLine
